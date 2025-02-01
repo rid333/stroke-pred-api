1b0efdc35141e4f6016a7980b8d29b08d140daf6
Al Ridho <alridho2003@gmail.com>
2025-02-01 16:37:22 +0800

    updated ap

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import StandardScaler
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://stroke-pred-api.vercel.app"],
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"],
